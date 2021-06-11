8024908cbda1abf3168e8c1a6614554a87e5a066
Francois Berenger <unixjunkie@sdf.org>
2021-06-11 14:43:43 +0900

    Test: real CLI

--- src/test.ml
+++ src/test.ml
@@ -1,11 +1,14 @@
 
 module A = BatArray
+module CLI = Minicli.CLI
 module IntMap = BatMap.Int
 module L = BatList
 module LO = Line_oriented
 module Log = Dolog.Log
 module RFC = Orf.RFC
 module S = BatString
+
+open Printf
 
 let label_of_string = int_of_string
 
@@ -31,23 +34,47 @@ let load_csv_file fn =
 let main () =
   Log.color_on ();
   Log.(set_log_level DEBUG);
-  let train_set = load_csv_file "data/train.csv" in
-  let _test_set = load_csv_file "data/test.csv" in
-  let ncores = 1 in
-  let ntrees = 1 in
+  let argc, args = CLI.init () in
+  if argc = 0 then
+    begin
+      printf "%s -tr train.csv \
+              -te test.csv \
+              -n <ntrees:int> \
+              -np <ncores:int>" Sys.argv.(0);
+      exit 0
+    end;
+  let train_set =
+    let train_set_fn = CLI.get_string ["-tr"] args in
+    load_csv_file train_set_fn in
+  let test_set =
+    let test_set_fn = CLI.get_string ["-te"] args in
+    load_csv_file test_set_fn in
+  let ncores = CLI.get_int_def ["-np"] args 1 in
+  let ntrees = CLI.get_int_def ["-n"] args 100 in
+  let feats_portion = CLI.get_float_def ["-feats"] args 1.0 in
+  let samples_portion = CLI.get_float_def ["-samps"] args 1.0 in
+  let min_node_size = CLI.get_int_def ["-min"] args 1 in
+  CLI.finalize();
+  let rng = BatRandom.State.make [|3141593|] in
   let model =
     RFC.(train
            ncores
-           (BatRandom.State.make [|3141593|])
+           rng
            Gini
            ntrees
-           (Float 0.1)
+           (Float feats_portion)
            17368
-           (Float 1.0)
-           1
+           (Float samples_portion)
+           min_node_size
            train_set) in
   let oob_preds = RFC.predict_OOB model train_set in
-  let mcc = RFC.mcc 1 oob_preds in
-  Log.info "OOB MCC: %f" mcc
+  let oob_mcc = RFC.mcc 1 oob_preds in
+  let preds = RFC.predict_many rng ncores model test_set in
+  let test_true_labels = A.map snd test_set in
+  let test_pred_labels = A.map fst preds in
+  let test_preds =
+    A.map2 (fun x y -> (x, y)) test_true_labels test_pred_labels in
+  let test_mcc = RFC.mcc 1 test_preds in
+  Log.info "OOB MCC: %f test MCC: %f" oob_mcc test_mcc
 
 let () = main ()
